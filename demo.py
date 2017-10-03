79c6353d2dcb1daa833e14f303b756a80e147d65
Elton Williams <EWilliams@melbro.co.za>
2017-10-03 11:22:01 +0200

    added spaces

--- demo.py
+++ demo.py
@@ -3,19 +3,20 @@ i=1
 while i<=150:
     print "Count at :",i
     if i == 150/2:
-        print "half way through, almost there"
+        print "/nhalf way through, almost there/n"
     i+=1
 
-print "this is now an acceptable amount"
+print "/n/n--------------------------------------"
+print "/nthis is now an acceptable amount"
 i-=1
-print "the amount now is",i
-print "now we will subtract 50 from",i
+print "/nthe amount now is",i
+print "/n/nnow we will subtract 50 from",i
 print i,"- 50"
 i-=50
-print "the amount now is",i
-print "now we will count backwards from",i
+print "/nthe amount now is",i
+print "/n/nnow we will count backwards from",i
 while i>=0:
     print "Count at :",i
     if i == 50:
-        print "half way through, almost there"
+        print "/nhalf way through, almost there/n"
     i-=1
