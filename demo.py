b158402eed45369239d37befb1c076889434b588
Elton Williams <EWilliams@melbro.co.za>
2017-10-03 11:08:47 +0200

    reduced count size and then counted backwards

--- demo.py
+++ demo.py
@@ -5,4 +5,11 @@ while i<=150:
     i+=1
 
 print "this is now an acceptable amount"
-    
+
+print "the amount now is ",i
+print "now we will subtract 50 from ",i
+print i," - 50"
+print "now we will count backwards from ",i
+while i>=0:
+    print "Count at :",i
+    i-=1
