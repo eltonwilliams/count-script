f7417b8872b73e162a65955728b4adcda4575619
Elton Williams <EWilliams@melbro.co.za>
2017-10-03 11:13:11 +0200

    removed extra spaced and fixed amount output to 150

--- demo.py
+++ demo.py
@@ -5,11 +5,13 @@ while i<=150:
     i+=1
 
 print "this is now an acceptable amount"
-
-print "the amount now is ",i
-print "now we will subtract 50 from ",i
-print i," - 50"
-print "now we will count backwards from ",i
+i-=1
+print "the amount now is",i
+print "now we will subtract 50 from",i
+print i,"- 50"
+i-=50
+print "the amount now is",i
+print "now we will count backwards from",i
 while i>=0:
     print "Count at :",i
     i-=1
